9544079e500414671a06384c9d2eec74cf776842
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-09-11 13:58:34 +0530

    Connected app to a database

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+
+const session_secret = "g63g3ix378772W&^";
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+app.use(
+  session({
+    secret: session_secret,
+  })
+);
+
+const db = mongoose.createConnection(
+  "mongodb+srv://ravi:ravi@dino.m8fqt.mongodb.net/Dino?retryWrites=true&w=majority"
+);
+
+const userSchema = new mongoose.Schema({
+  userName: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  dob: String,
+  sex: String,
+  country: String,
+  phNum: Number,
+});
+
+const userModel = db.model("user", userSchema);
 
 app.get("/", (req, res) => {
   res.send("Working");
